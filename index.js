b7b527763e6e53983ff2985b849795085633db89
Toni Iannucci <toni.iannucci@gmail.com>
2025-04-04 17:21:19 +0200

    Mise en place du bouton save tab et changement dans les functions

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 let myLeads = [];
-let oldLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const tabBtn = document.getElementById("tab-btn");
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   render(myLeads);
 }
+
+const tabs = [{ url: "https://www.linkedin.com/in/per-harald-borgen/" }];
+
+tabBtn.addEventListener("click", function () {
+  // Save the url instead of logging it out
+  console.log(tabs[0].url);
+});
 
 function render(leads) {
   let listItems = "";
